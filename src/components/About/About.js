b98aa5aa60b2b11f5b5f006aa1b3100b0178cca8
Sina Rafieefar <anisr95@yahoo.com>
2022-08-11 23:39:11 -0700

    Animated the landing page title and description

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,7 @@ const About = () => {
   return (
     <div className='about center'>
       {name && (
-        <h1>
+        <h1 className='greeting'>
           Hi, I am <span className='about__name'>{name}.</span>
         </h1>
       )}
@@ -18,6 +18,7 @@ const About = () => {
       {role && <h2 className='about__role'>A {role}.</h2>}
       <p className='about__desc'>{description && description}</p>
 
+        <div className='action__links'>
       <div className='about__contact center'>
         {resume && (
           <a href={myResume} target="_blank" rel="noopener noreferrer">
@@ -26,32 +27,33 @@ const About = () => {
             </span>
           </a>
         )}
+          {social && (
+            <>
+              {social.github && (
+                <a
+                  href={social.github}
+                  aria-label='github'
+                  className='link link--icon'
+                  target="_blank" rel="noopener noreferrer"
+                >
+                  <GitHubIcon />
+                </a>
+              )}
 
-        {social && (
-          <>
-            {social.github && (
-              <a
-                href={social.github}
-                aria-label='github'
-                className='link link--icon'
-                target="_blank" rel="noopener noreferrer"
-              >
-                <GitHubIcon />
-              </a>
-            )}
-
-            {social.linkedin && (
-              <a
-                href={social.linkedin}
-                aria-label='linkedin'
-                className='link link--icon'
-                target="_blank" rel="noopener noreferrer"
-              >
-                <LinkedInIcon />
-              </a>
-            )}
-          </>
-        )}
+              {social.linkedin && (
+                <a
+                  href={social.linkedin}
+                  aria-label='linkedin'
+                  className='link link--icon'
+                  target="_blank" rel="noopener noreferrer"
+                >
+                  <LinkedInIcon />
+                </a>
+              )}
+              
+            </>
+          )}
+        </div>
       </div>
     </div>
   )
